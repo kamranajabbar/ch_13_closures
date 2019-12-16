3b129431ca9acb24afe019189cab32200cb5cc95
Kamran jabbar <kamranajabbar@gmail.com>
2019-12-16 15:13:42 +0500

    Added features: closure annotations

---
+++ src/closures_annotations.rs
@@ -0,0 +1,17 @@
+fn add_one_v1(x: u32) -> u32 {
+    x+1
+}
+
+let add_one_v2 = |x:u32| -> u32 {
+    x+1
+};
+
+let add_one_v3 = |x| {
+    x+1
+};
+
+let add_one_v4 = |x| x+1;
+
+pub fn run() {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 //mod closures;
 //mod closures_with_arguments;
 //mod closures_used_in_real_app_function;
-mod closures_used_in_real_app_closure;
+//mod closures_used_in_real_app_closure;
+mod closures_annotations;
 
 fn main() {
     //closures::run();
     //closures_with_arguments::run();
     //closures_used_in_real_app_function::run();
-    closures_used_in_real_app_closure::run();
+    //closures_used_in_real_app_closure::run();
+    closures_annotations::run();
 }
